84b3aacd62f32aa4b3117a36f6064bf0dffcc521
Md. Saiful Islam <42247667+SaifulBUBT@users.noreply.github.com>
2026-05-25 08:35:20 +0600

    Transforming array items

--- Adding-interactivity/src/App.jsx
+++ Adding-interactivity/src/App.jsx
@@ -1,22 +1,25 @@
-import Button from "./Button";
-import Toolbar from "./Toolbar";
-import Form from "./components/Form";
-import Gallery from "./components/Gallery";
-import MovingDot from "./components/MovingDot";
+// import Button from "./Button";
+// import Toolbar from "./Toolbar";
+// import Form from "./components/Form";
+// import Gallery from "./components/Gallery";
+// import MovingDot from "./components/MovingDot";
 
-import List from "./Arrays_in_state/AddingItems";
-import RemoveItems from "./Arrays_in_state/RemoveItems";
+// import List from "./Arrays_in_state/AddingItems";
+// import RemoveItems from "./Arrays_in_state/RemoveItems";
+
+import TransformingItems from "./Arrays_in_state/TransformingItems";
 function App() {
   return (
     <div>
-      <h1>Welcome to the App!</h1>
+      {/* <h1>Welcome to the App!</h1>
       <Button />
       <Toolbar />
       <Gallery />
       <MovingDot />
       <Form />
       <List />
-      <RemoveItems />
+      <RemoveItems /> */}
+      <TransformingItems />
     </div>
   );
 }
