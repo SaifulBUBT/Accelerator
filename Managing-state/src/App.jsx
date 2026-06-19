99d0b0910d61fa77b69f044071726d78b9ae73ab
Md. Saiful Islam <42247667+SaifulBUBT@users.noreply.github.com>
2026-06-19 08:55:25 +0600

    connect the event handler to state

--- Managing-state/src/App.jsx
+++ Managing-state/src/App.jsx
@@ -1,11 +1,8 @@
-import "./App.css";
-
 import Form from "./components/Form";
 
 function App() {
   return (
     <>
-      <div className="App underline">Managing State with</div>
       <Form />
     </>
   );
--- Managing-state/src/components/Form.jsx
+++ Managing-state/src/components/Form.jsx
@@ -1,28 +1,62 @@
-export default function Form({ status = "empty" }) {
+import { useState } from "react";
+
+export default function Form() {
+  const [answer, setAnswer] = useState("");
+  const [error, setError] = useState(null);
+  const [status, setStatus] = useState("typing");
+
+  if (status === "success") {
+    return <h1>That's right!</h1>;
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setStatus("submitting");
+    try {
+      await submitForm(answer);
+      setStatus("success");
+    } catch (err) {
+      setStatus("typing");
+      setError(err);
+    }
+  }
+
+  function handleTextareaChange(e) {
+    setAnswer(e.target.value);
+    setError(null);
+  }
+
+  function submitForm(answer) {
+    // Pretend it's hitting the network.
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        let shouldError = answer.toLowerCase() !== "lima";
+        if (shouldError) {
+          reject(new Error("Good guess but a wrong answer. Try again!"));
+        } else {
+          resolve();
+        }
+      }, 1500);
+    });
+  }
+
   return (
     <>
       <h2>City quiz</h2>
       <p>
         In which city is there a billboard that turns air into drinkable water?
       </p>
-      <form>
+      <form onSubmit={handleSubmit}>
         <textarea
-          className="border"
-          placeholder="Your answer here"
+          value={answer}
+          onChange={handleTextareaChange}
           disabled={status === "submitting"}
         />
         <br />
-        <button
-          className="bg-blue-500 text-white px-4 py-2 rounded mt-2 cursor-pointer 
-          hover:bg-blue-600 disabled:bg-gray-400 disabled:cursor-not-allowed"
-          disabled={status === "submitting"}
-        >
+        <button disabled={answer.length === 0 || status === "submitting"}>
           Submit
         </button>
-
-        {status === "error" && (
-          <p className="Error">Good guess but a wrong answer. Try again!</p>
-        )}
+        {error !== null && <p className="Error">{error.message}</p>}
       </form>
     </>
   );
