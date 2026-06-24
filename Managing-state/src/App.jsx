5aaefe3ad52905a3d9b7b1ca0d2debe165d9507e
Md. Saiful Islam <42247667+SaifulBUBT@users.noreply.github.com>
2026-06-24 07:04:44 +0600

    Picture component classs state change

--- Managing-state/src/App.jsx
+++ Managing-state/src/App.jsx
@@ -1,9 +1,11 @@
 import Form from "./components/Form2";
+import Picture from "./components/Picture";
 
 function App() {
   return (
     <>
       <Form />
+      <Picture />
     </>
   );
 }
---
+++ Managing-state/src/components/Picture.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+import "../assets/picture.css";
+
+export default function Picture() {
+  const [isActive, setIsActive] = useState(false);
+
+  let backgroundClassName = "background";
+  let pictureClassName = "picture";
+
+  if (isActive) {
+    pictureClassName += " picture--active";
+  } else {
+    backgroundClassName += " background--active";
+  }
+
+  return (
+    <div className={backgroundClassName} onClick={() => setIsActive(false)}>
+      <img
+        className={pictureClassName}
+        onClick={(e) => {
+          e.stopPropagation();
+          setIsActive(true);
+        }}
+        alt="Rainbow houses in Kampung Pelangi, Indonesia"
+        src="https://react.dev/images/docs/scientists/5qwVYb1.jpeg"
+      />
+    </div>
+  );
+}
